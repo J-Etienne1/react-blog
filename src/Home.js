e4f5b25bf40ecd2e5028eb2e4ae6bd010acb5291
Jason <jasonetienne1@gmail.com>
2022-10-20 19:04:21 +0100

    added some event handling

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log("Hello there", e);
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("hello " + name);
+  };
+
   return (
     <div className="home">
-        <h2>Homepage</h2>
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          handleClickAgain("Jason");
+        }}
+      >
+        Click me again
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
