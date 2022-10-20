5f0fbf90fca9206bf8027685ba2f88eaeee9b5b4
Jason <jasonetienne1@gmail.com>
2022-10-20 19:36:57 +0100

    using map to loop through lists

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,39 @@ import React from "react";
 import { useState } from "react";
 
 const Home = () => {
-  //let name = "Jason";
-  const [name, setName] = useState('Jason')
-  const [age, setAge] = useState(38)
-
-  const handleClick = () => {
-    setName('Nosaj')
-    setAge(56)
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
-    <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
-    </div>
+  <div className="home">
+    {blogs.map((blog)=>(
+        <div className="blog-preview" key={blog.id}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+
+        </div>
+
+    ))}
+
+  </div>
   );
 };
 
